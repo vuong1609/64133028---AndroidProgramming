476362f8386f1a05e52996e74df83e1e4105ab08
vuong1609 <vuong.h.64cntt@ntu.edu.vn>
2025-04-20 18:59:02 +0700

    Fragment Dynamic

---
+++ FragmentDynamic/app/src/main/java/com/example/fragmentdynamic/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.fragmentdynamic;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.frameContentHolder, new ContentFragment() )
+                .add(R.id.frameFooterHolder, new FooterFragment() )
+
+                .commit();
+    }
+}
