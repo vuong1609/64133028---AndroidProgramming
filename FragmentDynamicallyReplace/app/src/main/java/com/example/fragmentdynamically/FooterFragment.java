7927eaacb28d8339d73f6caa813f57691b31624a
vuong1609 <vuong.h.64cntt@ntu.edu.vn>
2025-04-20 19:52:41 +0700

    Fragment Replace

---
+++ FragmentDynamicallyReplace/app/src/main/java/com/example/fragmentdynamically/FooterFragment.java
@@ -0,0 +1,62 @@
+package com.example.fragmentdynamically;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FooterFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v =inflater.inflate(R.layout.fragment_footer, container, false);
+
+        Button b1 =v.findViewById(R.id.button1);
+        Button b2 =v.findViewById(R.id.button2);
+        Button b3 =v.findViewById(R.id.button3);
+
+
+        FragmentManager fragmentManager = getParentFragmentManager();
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment1())
+                        .commit();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment2())
+                        .commit();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment3())
+                        .commit();
+            }
+        });
+        // Inflate the layout for this fragment
+        return v;
+    }
+}
